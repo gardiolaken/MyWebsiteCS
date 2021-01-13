984320372c5d09e1683b4b74fd530cbd0d75044d
kenneth gardiola <gardiolaken@gmail.com>
2021-01-13 06:52:08 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebsiteCS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return File("..\\Content\\Images\\RESUME.pdf", "application/pdf");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ProjectPage()
+        {
+            return View();            
+        } 
+
+
+    }
+}
---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebsiteCS.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Projects
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bst()
+        {
+            return View();
+        }
+    }
+}
